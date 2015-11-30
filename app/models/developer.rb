8e829c75527be5b643f73df1125e6923f2b31794
mario.souza <mario.souza@walmart.com>
2015-11-30 04:38:59 -0200

    added unit tests for Developer model

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,6 +1,4 @@
 class Developer < ActiveRecord::Base
-  has_many :projects
-
   validates :name, presence: true
   validates :age, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :github, presence: true, format: {with: URI.regexp}
